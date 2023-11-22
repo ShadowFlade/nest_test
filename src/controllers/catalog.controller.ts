0a5c871ccc0a870fc3f4ba48e53f8820bdeaae9f
Vladimir Drugov <wolffdrugov@yandex.ru>
2023-11-22 17:49:17 +0300

    deleting elements

--- src/controllers/catalog.controller.ts
+++ src/controllers/catalog.controller.ts
@@ -12,7 +12,12 @@ export type IProduct = {
 @Controller('catalog')
 export class CatalogController {
   constructor(private readonly CatalogService: CatalogService) {}
-
+  
+  
+  @Post('/delete/:id')
+  async deleteProduct(@Param('id') id: number) {
+    return await this.CatalogService.deleteProduct(id);
+  }
   @Get('/allSections')
   async getAllSections(){
     const data = await this.CatalogService.getAllSections();
@@ -31,17 +36,19 @@ export class CatalogController {
     });
   }
 
-  @Get('')
-  async getProducts(): Promise<IProduct[]> {
-    const data = await this.CatalogService.getProducts();
-    return data; 
-  }
+
 
 
 
   @Post('/add')
   addProduct(@Body() { name, description, price }) {
     this.CatalogService.addProduct({ name, description, price });
+  }
+
+  @Get('')
+  async getProducts(): Promise<IProduct[]> {
+    const data = await this.CatalogService.getProducts();
+    return data; 
   }
 
 }
