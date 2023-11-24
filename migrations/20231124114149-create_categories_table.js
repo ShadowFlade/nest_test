73f279f8060b1a3f79b7a88ec5ad44da6f20c066
Vladimir Drugov <wolffdrugov@yandex.ru>
2023-11-24 14:59:02 +0300

    categories migration

---
+++ migrations/20231124114149-create_categories_table.js
@@ -0,0 +1,51 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('products', {
+      id: {
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      description: {
+        type: Sequelize.STRING,
+        allowNull: true,
+      },
+      slug: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      subcategories: {
+        type: Sequelize.JSONB,
+        allowNull: true,
+      },
+      parentCategoryId: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'categories',
+          key: 'id',
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('categories');
+  },
+};
