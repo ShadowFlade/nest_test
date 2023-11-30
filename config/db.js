616e435ad71fba6efddfa04998679936ed6489cf
Vladimir Drugov <wolffdrugov@yandex.ru>
2023-11-30 16:26:19 +0300

    master:fixed modules issue

--- config/db.js
+++ config/db.js
@@ -1,5 +1,5 @@
 import { Sequelize } from 'sequelize';
-import { serializeConfig } from './config';
+import serializeConfig from './config.js';
 import { config } from 'dotenv';
 config();
 export const sequelize = new Sequelize(serializeConfig);
